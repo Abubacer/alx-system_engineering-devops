91e88c1db11a0dab75fcae51a24119acc9ec3dd3
Abubacer <azizdesignislife@gmail.com>
2023-10-02 22:36:04 +0000

    Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,66 @@
+# A Puppet manifest that installs and setups an nginx web server.
+# Creates a custom HTTP header response.
+# The name of the custom HTTP header: X-Served-By.
+# The value of the HTTP header: the hostname of the server is running on.
+
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+        add_header X-Served-By \$hostname;
+        location / {
+
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
