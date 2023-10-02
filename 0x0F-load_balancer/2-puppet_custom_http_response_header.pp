1de95216cb6bd0cd0d02a9901b22f434ff3bb673
Abubacer <azizdesignislife@gmail.com>
2023-10-02 22:47:57 +0000

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -33,12 +33,12 @@ file { 'Nginx default config file':
 "server {
         listen 80 default_server;
         listen [::]:80 default_server;
+        add_header X-Served-By $hostname;
                root /var/www/html;
 
         index index.html index.htm index.nginx-debian.html;
 
         server_name _;
-        add_header X-Served-By $hostname;
         location / {
 
                 try_files \$uri \$uri/ =404;
